243d80505a9ed31e949318affbed3eea0381837f
heaven <porori0703@ewhain.net>
2022-02-05 15:26:30 +0900

    Initial commit

---
+++ src/test/java/ewha/nlsushi/newsum/NewsumApplicationTests.java
@@ -0,0 +1,13 @@
+package ewha.nlsushi.newsum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
