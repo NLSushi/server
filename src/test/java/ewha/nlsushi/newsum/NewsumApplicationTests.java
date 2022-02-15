d00f8f671547eb88a0ec0e2bb4b20cc03a02e81f
heaven <porori0703@ewhain.net>
2022-02-15 18:54:17 +0900

    무중단배포를 위한 profile 설정 변경

--- src/test/java/ewha/nlsushi/newsum/NewsumApplicationTests.java
+++ src/test/java/ewha/nlsushi/newsum/NewsumApplicationTests.java
@@ -1,13 +1,26 @@
 package ewha.nlsushi.newsum;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class NewsumApplicationTests {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    public void Profile확인(){
+        //when
+        String profile = this.restTemplate.getForObject("/profile",String.class);
+        //then
+        Assertions.assertThat(profile).isEqualTo("local");
     }
 
 }
