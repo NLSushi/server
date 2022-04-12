6140dac7c7ef512e4664e6ef21b9b6042447cfef
heaven <porori0703@ewhain.net>
2022-04-12 13:34:44 +0900

    member post mapping requestbody 형태로 수정

--- src/main/java/ewha/nlsushi/newsum/api/MemberApiController.java
+++ src/main/java/ewha/nlsushi/newsum/api/MemberApiController.java
@@ -6,7 +6,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
@@ -15,8 +18,11 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
-    @PostMapping(value = "api/{userId}/signup")
-    public Member signup(@PathVariable("userId")String userId){
-       return memberService.signup(userId);
+    @PostMapping(value = "api/signup")
+    public Member signup(@RequestBody @Valid SignupRequest request)
+    { return memberService.signup(request);
     }
+
+
+
 }
