ebf0d04947d3abae7af9353cccbe3e26c329dbb3
heaven <porori0703@ewhain.net>
2022-04-19 10:22:38 +0900

    member post mapping requestbody 형태로 수정

--- src/main/java/ewha/nlsushi/newsum/api/SignupRequest.java
+++ src/main/java/ewha/nlsushi/newsum/api/SignupRequest.java
@@ -2,14 +2,17 @@ package ewha.nlsushi.newsum.api;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignupRequest {
 
     @NotNull
     String userId;
+
 }
 
